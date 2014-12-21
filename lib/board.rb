1029daba5cfe3de658a885700e70ae0ff81f0b29
Hannah Carney <hannahcarneyart@gmail.com>
2014-12-21 18:23:16 +0000

    built a grid

--- lib/board.rb
+++ lib/board.rb
@@ -5,15 +5,18 @@ class Board
   def initialize 
     # @contents = conte
     array = []
-    @contents = Array.new(10){Cell.new}.each.with_index(1) {|cell, index| array << "#{cell} => #{index}"}
-    puts array.zip("A".."J").to_a {|a| puts a.join}
+    @contents = @grid.each {|key| puts key{Cell.new}}
+    # {|cell, grid| array << "#{cell} => #{grid}"}
 
   end
   
-  def grid
-    columns = (["A"]*10+["B"]*10+["C"]*10+["D"]*10+["E"]*10+["F"]*10+["G"]*10+["H"]*10+["I"]*10+["J"]*10).to_a {|a| puts a}
+  def grid (rows, columns)
+    columns = (["A"]*10+["B"]*10+["C"]*10+["D"]*10+["E"]*10+["F"]*10+["G"]*10+["H"]*10+["I"]*10+["J"]*10).to_a
     rows = (1..10).to_a {|num| puts num}*10
-    columns.zip(rows) {|a| puts a.join}
+    grid = []
+    grid << columns.zip(rows.flatten)
+    @grid = columns.zip(rows) {|key| puts key.join}.to_a
+
   end
          
 
